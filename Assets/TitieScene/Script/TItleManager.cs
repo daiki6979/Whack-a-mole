4ef31b0559c6141e83b982c4c4c899c49e1805d0
daiki6979 <231205148@ccmailg.meijo-u.ac.jp>
2026-01-23 15:57:34 +0900

    update

--- Assets/TitieScene/Script/TItleManager.cs
+++ Assets/TitieScene/Script/TItleManager.cs
@@ -7,9 +7,22 @@ public class TItleManager : MonoBehaviour
     public CanvasGroup fadeCanvasGroup;
     public float fadeDuration = 1f;
 
+    bool istarting = false;
+
     void Start()
     {
         StartCoroutine(FadeIn());
+    }
+
+    void Update()
+    {
+        if (istarting) return;
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            StartGame();
+        }
+        CheckPullByZValue();
     }
 
     IEnumerator FadeIn()
@@ -39,5 +52,38 @@ public class TItleManager : MonoBehaviour
             yield return null;
         }
         SceneManager.LoadScene("SampleScene");
+    }
+
+    void CheckPullByZValue()
+    {
+        if (Recelver.Instance == null) return;
+
+        float zAcc = Recelver.Instance.acc.z;
+
+        //初期のZの値を取得（基準値）
+        if (!isBaseSet)
+        {
+            baseZ = zAcc;
+            isBaseSet = true;
+            return;
+        }
+
+        float deltaZ = zAcc - baseZ;
+        Debug.Log(deltaZ);
+
+
+        //閾値を超えたら
+        if (canPullByAcc && deltaZ < pullThresholdZ)
+        {
+            StartGame();
+            canPullByAcc = false;
+        }
+
+        //加速度の閾値が戻る
+        if (zAcc > 1.0f)
+        {
+            canPullByAcc = true;
+        }
+
     }
 }
